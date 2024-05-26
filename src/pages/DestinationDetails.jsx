3d1d45b2142095a02fca6aa96f421ba465e48ff0
Kunal Sen <kunalsen7083@gmail.com>
2024-05-26 15:02:19 +0530

    DestinationDetails.jsx

--- src/pages/DestinationDetails.jsx
+++ src/pages/DestinationDetails.jsx
@@ -1,30 +1,85 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import { useParams, useSearchParams } from 'react-router-dom'
+import { URL } from '../App'
+import he from "he"
+import striptags from 'striptags'
 
-const DestinationDetails = ({ data }) => {
-    console.log(data);
+async function fetchDestByID(id,name,setData) {
+    try {
+      console.log(id,name)
+      const response = await fetch(`${URL}/getSite/${id}?name=${name}`)
+      const sites = await response.json()
+      setData(sites)
+      console.log(sites);
+      return sites
+    } catch (err) {
+      console.log(err)
+      throw Error('Error Occured')
+    }
+  }
+
+
+
+const DestinationDetails = () => {
+    // console.log(data);
+    
+    const {id} = useParams()
+    const [searchParams,setSearchParams] = useSearchParams()
+    const nameQ = searchParams.get('name')
+    const [data,setData] = useState({})
+    const[flora,setFlora] = useState([])
+    const[fauna,setFauna] = useState([])
+
+    const [error,setError] = useState(false)
+
+    useEffect(() => {
+      const getSites = async () => {
+        try {
+          const sites = await fetchDestByID(id,nameQ,setData)
+          setFlora(sites.flora)
+          setFauna(sites.fauna)
+        } catch (e) {
+          console.log(e)
+          setError(true)
+          // setError(true)
+        }
+      }
+      getSites()
+      // setIsLoading(false)
+    }, [])
+    
+
+
     return (
         <div>
             <div className="container grid grid-cols-1 md:grid-cols-2 gap-8 bg-gray-100 dark:bg-[#111111] dark:text-white py-10">
                 <div>
-                    <img className="h-full md:h-[600px] rounded" src="https://www.tourmyindia.com/blog//wp-content/uploads/2015/11/best-things-to-do-in-coorg.jpg" alt="" />
+                    <img className="h-full" src="https://www.tourmyindia.com/blog//wp-content/uploads/2015/11/best-things-to-do-in-coorg.jpg" alt="" />
                 </div>
                 <div>
-                    <h1 className="text-3xl font-bold text-[#689A5E] mb-5">Coorg, Karnataka</h1>
-                    <p className='mb-5'>Located amidst imposing mountains in Karnataka with a perpetually misty landscape, Coorg is a popular coffee producing hill station. It is popular for its beautiful green hills and the streams cutting right through them. It also stands as a popular destination because of its culture and people. The Kodavas, a local clan specializing in martial arts, are especially notable for their keen hospitality.  <br />
-
-
-
-                        Coorg, officially known as Kodagu, is the most affluent hill station in Karnataka. It is well known for its breathtakingly exotic scenery and lush greenery. Forest covered hills, spice and coffee plantations only add to the landscape. Madikeri is the region's centre point with all transportation for getting around starting from here. On a visit to Coorg, cover the beautiful towns like Virajpet, Kushalnagar, Gonikoppal, Pollibetta, and Somwarpet, and experience the beautiful concept of "homestays" to make your experience more memorable! </p>
-                    <div>
-                        <h1 className="text-3xl font-bold text-[#689A5E] mb-3">Flora</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dolorum sapiente natus porro debitis at laborum rerum hic omnis molestias expedita, modi neque id, aut quod corrupti unde ipsa voluptas.</p>
-                    </div>
-                    <div>
-                        <h1 className="text-3xl font-bold text-[#689A5E] mt-4 mb-3">Fauna</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dolorum sapiente natus porro debitis at laborum rerum hic omnis molestias expedita, modi neque id, aut quod corrupti unde ipsa voluptas.</p>
-                    </div>
+                    <h1 className="text-3xl font-bold text-[#689A5E] mb-5">{data.site}</h1>
+                    <>{striptags(data.short_description)}</>
+                    
                 </div>
-
+                <div>
+                        <h1 className="text-3xl font-bold text-[#689A5E] mb-3">Flora</h1>
+                        {flora.map((flora)=>{
+                            return(
+                                <p>{flora}</p>
+                            )
+                        })}
+                  </div>
+                  <div>
+                        <h1 className="text-3xl font-bold text-[#689A5E] mb-3">Fauna</h1>
+                        {fauna.map((fauna)=>{
+                            return(
+                                <p>{fauna}</p>
+                            )
+                        })}
+                    </div>
+                    <div>
+                      <p>Location : <a href={`https://www.google.com/maps/place/${data.latitude}+-${data.longitude}`}>Location Map</a></p>
+                    </div>
             </div>
         </div>
     )
